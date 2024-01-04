689a4d04b5fef34d6abecdaa957c4025f593763b
MIN UK JUNG <alsdnrdl001@gmail.com>
2024-01-04 16:46:44 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "parting",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "parting developers team",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
